938c8099f0445fadd6437b43c707741402103ff2
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-18 12:46:10 -0500

    Added shellcheck to my vim+linters config.

--- config/nvim/linters.lua
+++ config/nvim/linters.lua
@@ -1,8 +1,12 @@
 local dlsconfig = require 'diagnosticls-configs'
+local shellcheck = require 'diagnosticls-configs.linters.shellcheck'
 local swiftlint = require 'diagnosticls-configs.linters.swiftlint'
 
 dlsconfig.setup {
+  ['sh'] = {
+    linter = shellcheck
+  },
   ['swift'] = {
     linter = swiftlint
-  }
+  },
 }
