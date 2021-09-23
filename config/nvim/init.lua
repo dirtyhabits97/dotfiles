9fda3ef49d4e3400f7221afbcf18024005590f9e
dirtyhabits97 <gonzalorehu@gmail.com>
2021-09-22 21:08:40 -0500

    Fixed an issue with lsp warnings and errors offcoloring.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,12 @@
 require('general')
 require('plugins')
 
+-- Colors have to be set before the lsp
+require('theme')
+
 require('lsp')
 require('completion')
 require('diagnostics')
 require('linters')
-
-require('theme')
 
 vim.cmd 'source $HOME/.config/nvim/mappings.vim'
