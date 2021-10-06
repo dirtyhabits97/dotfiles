c3079b6deea64c02e3d11a941cb1c8c3f5e19abe
dirtyhabits97 <gonzalorehu@gmail.com>
2021-10-06 00:15:40 -0500

    Added cpplint.

--- config/nvim/lua/linters.lua
+++ config/nvim/lua/linters.lua
@@ -1,8 +1,12 @@
 local dlsconfig = require 'diagnosticls-configs'
+local cpplint = require 'diagnosticls-configs.linters.cpplint'
 local shellcheck = require 'diagnosticls-configs.linters.shellcheck'
 local swiftlint = require 'diagnosticls-configs.linters.swiftlint'
 
 dlsconfig.setup {
+  ['cpp'] = {
+    linter = cpplint
+  },
   ['sh'] = {
     linter = shellcheck
   },
