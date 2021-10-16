ea6f6280a7bbfa0218ea72debb4dd95aafcdd4bf
dirtyhabits97 <gonzalorehu@gmail.com>
2021-10-16 16:16:22 -0500

    More improvements to vim / tmux settings.

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -66,6 +66,7 @@ vim.g['NERDDefaultAlign'] = 'left'
 -- Tabs
 vim.g['airline#extensions#tabline#enabled'] = 1
 vim.g['airline#extensions#tabline#formatter'] = 'unique_tail'
+vim.g['airline#extensions#tabline#buffer_nr_show'] = 1
 
 -- FZF
 vim.g['fzf_action'] = {
