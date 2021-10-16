b0e4bd922c69aa5a6dab10ca704dc57fc25856e1
dirtyhabits97 <gonzalorehu@gmail.com>
2021-10-16 18:56:44 -0500

    Added luacheck to linters config.

--- config/nvim/lua/linters.lua
+++ config/nvim/lua/linters.lua
@@ -1,11 +1,17 @@
 local dlsconfig = require 'diagnosticls-configs'
+
 local cpplint = require 'diagnosticls-configs.linters.cpplint'
+-- luarocks install luacheck
+local luacheck = require 'diagnosticls-configs.linters.luacheck'
 local shellcheck = require 'diagnosticls-configs.linters.shellcheck'
 local swiftlint = require 'diagnosticls-configs.linters.swiftlint'
 
 dlsconfig.setup {
   ['cpp'] = {
     linter = cpplint
+  },
+  ['lua'] = {
+    linter = luacheck
   },
   ['sh'] = {
     linter = shellcheck
