0c8127851d8c1851a0fc356b68cc774f3bb53080
dirtyhabits97 <gonzalorehu@gmail.com>
2021-10-17 18:47:40 -0500

    Updated cmp config.

--- config/nvim/lua/completion.lua
+++ config/nvim/lua/completion.lua
@@ -28,7 +28,11 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({
+      -- TODO: test this behavior.
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true
+    }),
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
