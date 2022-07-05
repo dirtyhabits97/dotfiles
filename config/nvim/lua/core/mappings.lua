5f522af7daf9a848388ce44789c696fc98556e80
dirtyhabits97 <gonzalorehu@gmail.com>
2022-07-04 20:25:54 -0500

    Migrated some mappings.

--- config/nvim/lua/core/mappings.lua
+++ config/nvim/lua/core/mappings.lua
@@ -1,11 +1,45 @@
 ------------------------------- Mappings ------------------------------
 
--- Easier reload
+-- Leader key
+vim.g.mapleader = " "
+
+local map = vim.api.nvim_set_keymap
+
+map('n', '<C-n>', [[:NERDTreeToggle<cr>]], { noremap = true })
+map('n', '<C-m>', [[:NERDTreeFind<cr>]], { noremap = true })
+
+-- Easier code comment
+map('n', '<C-_>', [[<Plug>NERDCommenterToggle]], {})
+map('v', '<C-_>', [[<Plug>NERDCommenterToggle<Bar>gv]], {})
+
+-- Easier write and quit
+vim.api.nvim_create_user_command(
+  'W', [[write]], { nargs = 0 }
+)
+vim.api.nvim_create_user_command(
+  'Q', [[quit]], { nargs = 0 }
+)
+map('c', 'Wq', 'wq', {})
+map('c', 'WQ', 'wq', {})
+
+-- Easier escape
+map('i', 'jj', '<ESC>', { noremap = true })
+map('i', 'kk', '<ESC>', { noremap = true })
+
+-- Easier navigation with wrapped lines
+map('n', 'j', 'gj', { noremap = true })
+map('n', 'k', 'gk', { noremap = true })
+
+-- Easier navigation between buffers
+map('n', '<leader>1', [[:bp<cr>]], {})
+map('n', '<leader>2', [[:bn<cr>]], {})
+
+-- Custom commands
 vim.api.nvim_create_user_command(
   'Reload',
   function()
     for name, _ in pairs(package.loaded) do
-      if name:match('^core') or name:match('^extensions') then
+      if name:match('^core') or name:match('^extension') then
         package.loaded[name] = nil
       end
     end
@@ -16,5 +50,11 @@ vim.api.nvim_create_user_command(
   { nargs = 0 }
 )
 
--- Leader key
-vim.g.mapleader = " "
+-- TODO: implement this
+-- vim.api.nvim_create_user_command(
+--   'Config',
+--   function()
+--     vim.api.ope
+--   end,
+--   { nargs = 0 }
+-- )
--- config/nvim/mappings.vim
+++ config/nvim/mappings.vim
@@ -8,31 +8,31 @@ command! Config :edit ~/.config/nvim/mappings.vim
 "let g:mapleader=","
 " let g:mapleader="\<Space>"
 
-nnoremap <C-n> :NERDTreeToggle<cr>
-nnoremap <C-m> :NERDTreeFind<cr>
+" nnoremap <C-n> :NERDTreeToggle<cr>
+" nnoremap <C-m> :NERDTreeFind<cr>
 
 " Easier code comment
-nmap <C-_> <Plug>NERDCommenterToggle
-vmap <C-_> <Plug>NERDCommenterToggle<Bar>gv
+" nmap <C-_> <Plug>NERDCommenterToggle
+" vmap <C-_> <Plug>NERDCommenterToggle<Bar>gv
 
 " Use tab to navigate through the pop up menu
-inoremap <expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
-inoremap <expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<TAB>"
+" inoremap <expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+" inoremap <expr> <S-TAB> pumvisible() ? "\<C-p>" : "\<TAB>"
 
 " Easier write and quit
-command! W write
-command! Q quit
-inoremap jj <ESC>
-inoremap kk <ESC>
-cmap Wq wq
+" command! W write
+" command! Q quit
+" inoremap jj <ESC>
+" inoremap kk <ESC>
+" cmap Wq wq
 
 " Easier navigation with wrapped lines
-nnoremap j gj
-nnoremap k gk
+" nnoremap j gj
+" nnoremap k gk
 
 " Easier navigation between buffers
-nmap <leader>1 :bp<cr>
-nmap <leader>2 :bn<cr>
+" nmap <leader>1 :bp<cr>
+" nmap <leader>2 :bn<cr>
 
 " Easier merge solving with fugitive
 nmap <leader>gj :diffget //3<CR>
