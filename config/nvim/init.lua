660dfb1c400afebe865565886991481ceb9be81a
dirtyhabits97 <gonzalorehu@gmail.com>
2022-07-26 00:59:55 -0500

    Created core/init.lua.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,1 @@
-require('core.general')
-require('core.plugins')
-require('core.mappings')
--- Colors have to be set before the lsp
-require('core.theme')
-require('core.lsp')
+require('core')
---
+++ config/nvim/lua/core/init.lua
@@ -0,0 +1,6 @@
+require('core.general')
+require('core.plugins')
+require('core.mappings')
+-- Colors have to be set before the lsp
+require('core.theme')
+require('core.lsp')
