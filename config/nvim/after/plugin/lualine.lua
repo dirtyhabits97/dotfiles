a64bfd1b5761e8c774e90f444145fb5f5a04bccd
dirtyhabits97 <gonzalorehu@gmail.com>
2022-08-06 00:27:59 -0500

    Explicitely define colors.

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -6,12 +6,10 @@ require('lualine').setup {
         mode = 4,
         buffers_color = {
           -- Same values as the general color option can be used here.
-          active = 'lualine_a_insert', -- Color for active buffer.
+          active = { gui = 'bold', fg = '#2c3039', bg = '#a7d685' }, -- Color for active buffer.
           inactive = { fg = '#a7d685', bg = '#282c34' }, -- Color for inactive buffer.
         },
       }
     },
   }
 }
-
-vim.cmd('colorscheme onehalfdark')
