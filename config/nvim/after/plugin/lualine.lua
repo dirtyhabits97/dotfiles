71af243e65101d2f4374f30a22072b507d13bfbe
dirtyhabits97 <gonzalorehu@gmail.com>
2022-08-06 00:19:26 -0500

    Improved color for active and inactive buffers.

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -6,8 +6,8 @@ require('lualine').setup {
         mode = 4,
         buffers_color = {
           -- Same values as the general color option can be used here.
-          active = 'lualine_a_normal', -- Color for active buffer.
-          inactive = 'lualine_a_replace', -- Color for inactive buffer.
+          active = 'lualine_a_insert', -- Color for active buffer.
+          inactive = { fg = '#a7d685', bg = '#282c34' }, -- Color for inactive buffer.
         },
       }
     },
