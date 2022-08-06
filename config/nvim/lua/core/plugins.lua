5d70c9502bbb181706f7c350c80eed265d71f3e9
Gonzalo <gonzalorehu@gmail.com>
2022-08-06 00:28:24 -0500

    Statusline (#7)
    * Comment airline code.
    
    * Added more icons and config.
    
    * Improved color for active and inactive buffers.
    
    * Removed uncommented airline code.
    
    * Explicitely define colors.

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -15,8 +15,7 @@ Plug('nvim-treesitter/nvim-treesitter', { -- Language
 Plug 'preservim/nerdtree'                 -- File browsing
 Plug 'preservim/nerdcommenter'            -- Easier comment
 Plug 'christoomey/vim-tmux-navigator'     -- Vim + Tmux
-Plug 'vim-airline/vim-airline'            -- Status bar
-Plug 'vim-airline/vim-airline-themes'
+Plug 'nvim-lualine/lualine.nvim'          -- Status bar
 Plug 'tpope/vim-surround'                 -- Surround text objects
 Plug 'tpope/vim-commentary'               -- Comment code
 Plug('junegunn/fzf', {                    -- Fuzzy find
@@ -53,6 +52,7 @@ Plug 'iamcco/diagnostic-languageserver'   -- Needed for linters
 Plug 'creativenull/diagnosticls-configs-nvim'   -- Linters
 
 Plug 'ryanoasis/vim-devicons'             -- Icons
+Plug 'kyazdani42/nvim-web-devicons'       -- Icons
 Plug('sonph/onehalf', { rtp = 'vim' })    -- Colors
 
 Plug 'lewis6991/impatient.nvim'           -- Performance
@@ -82,11 +82,6 @@ vim.g['NERDCustomDelimiters'] = {
 
 -- Align the comments
 vim.g['NERDDefaultAlign'] = 'left'
-
--- Tabs
-vim.g['airline#extensions#tabline#enabled'] = 1
-vim.g['airline#extensions#tabline#formatter'] = 'unique_tail'
-vim.g['airline#extensions#tabline#buffer_nr_show'] = 1
 
 -- FZF
 vim.g['fzf_action'] = {
