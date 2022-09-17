b67cdc37444f556e7dbe2386d32cc19f9be427da
dirtyhabits97 <gonzalorehu@gmail.com>
2022-09-17 13:46:36 -0500

    Added custom color for "MARK" comments.

--- config/nvim/after/plugin/todo-comments.lua
+++ config/nvim/after/plugin/todo-comments.lua
@@ -1,6 +1,9 @@
 local todoComments = require('todo-comments')
 todoComments.setup {
   keywords = {
-    MARK = { icon = " ", color = "hint" }
+    MARK = { icon = " ", color = "mark" }
+  },
+  colors = {
+    mark = { "#61afef" }
   }
 }
