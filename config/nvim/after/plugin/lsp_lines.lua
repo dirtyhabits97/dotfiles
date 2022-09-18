b0c0917fe8c8ff63e4d9789638e13ad68d366718
dirtyhabits97 <gonzalorehu@gmail.com>
2022-09-17 20:48:55 -0500

    Added basic configuration to lsp-lines.

--- config/nvim/after/plugin/lsp_lines.lua
+++ config/nvim/after/plugin/lsp_lines.lua
@@ -1,1 +1,10 @@
-require('lsp_lines').setup()
+-- doc: https://git.sr.ht/~whynothugo/lsp_lines.nvim
+
+local lsp_lines = require('lsp_lines')
+lsp_lines.setup()
+
+vim.diagnostic.config({ 
+  virtual_lines = {
+    only_current_line = true 
+  } 
+})
