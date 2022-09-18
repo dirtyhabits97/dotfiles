63192112c200fafb4bc8cc5a21ce9d08c8171917
dirtyhabits97 <gonzalorehu@gmail.com>
2022-09-17 20:55:11 -0500

    fixup! Added basic configuration to lsp-lines.

--- config/nvim/after/plugin/lsp_lines.lua
+++ config/nvim/after/plugin/lsp_lines.lua
@@ -3,8 +3,7 @@
 local lsp_lines = require('lsp_lines')
 lsp_lines.setup()
 
-vim.diagnostic.config({ 
-  virtual_lines = {
-    only_current_line = true 
-  } 
+-- Disable virtual_text since it's redundant due to lsp_lines.
+vim.diagnostic.config({
+  virtual_text = false,
 })
