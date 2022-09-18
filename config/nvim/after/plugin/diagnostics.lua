af9b4ee903265d39a3487891b07a10486837aef4
dirtyhabits97 <gonzalorehu@gmail.com>
2022-09-17 20:56:41 -0500

    Move lsp_lines config to diagnostic file.

--- config/nvim/after/plugin/diagnostics.lua
+++ config/nvim/after/plugin/diagnostics.lua
@@ -4,3 +4,12 @@ local trouble = require('trouble')
 trouble.setup {
   icons = false, -- use devicons for filenames
 }
+
+-- doc: https://git.sr.ht/~whynothugo/lsp_lines.nvim
+local lsp_lines = require('lsp_lines')
+lsp_lines.setup()
+
+-- Disable virtual_text since it's redundant due to lsp_lines.
+vim.diagnostic.config({
+  virtual_text = false,
+})
--- config/nvim/after/plugin/lsp_lines.lua
+++
@@ -1,9 +0,0 @@
--- doc: https://git.sr.ht/~whynothugo/lsp_lines.nvim
-
-local lsp_lines = require('lsp_lines')
-lsp_lines.setup()
-
--- Disable virtual_text since it's redundant due to lsp_lines.
-vim.diagnostic.config({
-  virtual_text = false,
-})
