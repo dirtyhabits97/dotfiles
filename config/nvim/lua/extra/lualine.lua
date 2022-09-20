fbd009266e2da4c873b7c7844c043a6e87107204
dirtyhabits97 <gonzalorehu@gmail.com>
2022-09-20 02:27:29 -0500

    Added icons.

--- config/nvim/lua/extra/lualine.lua
+++ config/nvim/lua/extra/lualine.lua
@@ -1,5 +1,6 @@
 -- source: https://github.com/nvim-lualine/lualine.nvim
 
+local navic = require("nvim-navic")
 local lualine = require('lualine')
 lualine.setup {
   -- tabline = {
@@ -15,4 +16,45 @@ lualine.setup {
   --     }
   --   },
   -- }
+  sections = {
+    lualine_c = {
+      { navic.get_location, cond = navic.is_available },
+    }
+  }
+}
+
+-- TODO: move this to its own file
+navic.setup {
+    icons = {
+        File          = " ",
+        Module        = " ",
+        Namespace     = " ",
+        Package       = " ",
+        Class         = " ",
+        Method        = " ",
+        Property      = " ",
+        Field         = " ",
+        Constructor   = " ",
+        Enum          = "練",
+        Interface     = "練",
+        Function      = " ",
+        Variable      = " ",
+        Constant      = " ",
+        String        = " ",
+        Number        = " ",
+        Boolean       = "◩ ",
+        Array         = " ",
+        Object        = " ",
+        Key           = " ",
+        Null          = "ﳠ ",
+        EnumMember    = " ",
+        Struct        = " ",
+        Event         = " ",
+        Operator      = " ",
+        TypeParameter = " ",
+    },
+    highlight = false,
+    separator = " > ",
+    depth_limit = 0,
+    depth_limit_indicator = "..",
 }
