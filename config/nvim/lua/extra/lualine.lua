9ae8f483458ffbb123451a7e3bc1c480fdf35040
dirtyhabits97 <gonzalorehu@gmail.com>
2022-10-23 22:58:00 -0500

    Update lualine config.

--- config/nvim/lua/extra/lualine.lua
+++ config/nvim/lua/extra/lualine.lua
@@ -2,17 +2,12 @@
 
 local lualine = require('lualine')
 lualine.setup {
-  -- tabline = {
-  --   lualine_a = {
-  --     {
-  --       'buffers',
-  --       mode = 4,
-  --       buffers_color = {
-  --         -- Same values as the general color option can be used here.
-  --         active = { gui = 'bold', fg = '#2c3039', bg = '#a7d685' }, -- Color for active buffer.
-  --         inactive = { fg = '#a7d685', bg = '#282c34' }, -- Color for inactive buffer.
-  --       },
-  --     }
-  --   },
-  -- }
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    }
+  }
 }
