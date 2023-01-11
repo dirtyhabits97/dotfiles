f504d3234ba04d36939c5a8141380d0de13649bc
dirtyhabits97 <gonzalorehu@gmail.com>
2023-01-10 23:53:29 -0500

    Moved treesitter code.

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -1,24 +1,5 @@
 ------------------------------- Plugins ------------------------------
 
--- TODO: remove this soon
--- Leader key
-vim.g.mapleader = " "
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup('plugins')
-
 -- local Plug = vim.fn['plug#']
 
 -- vim.call('plug#begin', '~/.nvim/plugged')
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,30 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate'
+    build = ':TSUpdate',
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = false,
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "json",
+        "javascript",
+        "lua",
+        "python",
+        "ruby",
+        -- currently, the swift parser is slowing down redraws.
+        -- "swift",
+        "tsx",
+        "toml",
+        "vim",
+        "yaml",
+      },
+    }
   },
 }
