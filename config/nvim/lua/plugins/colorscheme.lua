3f1110f0d50f2965db5e735dd29aca035e747260
dirtyhabits97 <gonzalorehu@gmail.com>
2023-01-11 01:21:42 -0500

    Switch to tokyonight theme for now.

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,10 @@
 return {
   {
-    "sonph/onehalf",
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function(plugin)
-      -- Solves: https://github.com/folke/lazy.nvim/issues/183
-      vim.opt.rtp:append(plugin.dir .. "/vim")
-
-      vim.cmd('colorscheme onehalfdark')
-
-      vim.cmd [[
-      if exists('+termguicolors')
-        let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
-        let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
-        set termguicolors
-        endif
-        ]]
+    config = function()
+      vim.cmd('colorscheme tokyonight')
     end,
   },
 }
