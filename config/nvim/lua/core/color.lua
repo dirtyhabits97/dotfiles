48ea65bd191f105974696f7c90d0429f65aa3f0b
dirtyhabits97 <gonzalorehu@gmail.com>
2023-01-21 23:57:01 -0500

    Updated color configuration.

---
+++ config/nvim/lua/core/color.lua
@@ -0,0 +1,10 @@
+vim.go.t_Co = '256'
+vim.cmd('colorscheme tokyonight')
+
+vim.cmd[[
+  if exists('+termguicolors')
+    let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+    let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+    set termguicolors
+  endif
+]]
