58b446ad0d9dfb7dfaa5c7dec2af6b6a1d024ee3
dirtyhabits97 <gonzalorehu@gmail.com>
2023-01-22 22:15:51 -0500

    Added methods to keymap module.

---
+++ config/nvim/lua/utils/keymap.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+M.cmap = function(alias, cmd)
+  vim.api.nvim_set_keymap('c', alias, cmd, {})
+end
+
+M.nmap = function(alias, cmd)
+  vim.api.nvim_set_keymap('n', alias, cmd, {})
+end
+
+M.vmap = function(alias, cmd)
+  vim.api.nvim_set_keymap('v', alias, cmd, {})
+end
+
+M.inoremap = function(alias, cmd)
+  vim.api.nvim_set_keymap('i', alias, cmd, { noremap = true })
+end
+
+M.nnoremap = function(alias, cmd)
+  vim.api.nvim_set_keymap('n', alias, cmd, { noremap = true })
+end
+
+M.command = function(name, f)
+  vim.api.nvim_create_user_command(name, f, { nargs = 0 })
+end
+
+return M
