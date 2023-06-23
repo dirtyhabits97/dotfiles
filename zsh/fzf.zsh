628794870f940e77a3ac0aeb34930c3e12ab4c33
dirtyhabits97 <gonzalorehu@gmail.com>
2023-06-22 20:02:48 -0500

    Disabled a zsh and lldb config.

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,8 +1,8 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 export FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"
-export FZF_DEFAULT_OPTS="--height 50% \
-  --preview='bat --color=always --style=plain --theme=OneHalfDark {}' \
-  --bind shift-up:preview-page-up,shift-down:preview-page-down"
+#export FZF_DEFAULT_OPTS="--height 50% \
+#  --preview='bat --color=always --style=plain --theme=OneHalfDark {}' \
+#  --bind shift-up:preview-page-up,shift-down:preview-page-down"
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
