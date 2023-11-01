2c61c2a01c7f9a13687e7a47824ea468ee98c061
dirtyhabits97 <gonzalorehu@gmail.com>
2023-11-01 18:20:34 -0500

    Update nvim_tree.

--- config/nvim/lua/ide/file_explorer.lua
+++ config/nvim/lua/ide/file_explorer.lua
@@ -1,21 +1,28 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- Remove / Replace default mappings
+  vim.keymap.set('n', '<C-[>', api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', '<C-t>', api.node.open.vertical, opts('Open: Vertical split'))
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical split'))
+  -- -- New mappings
+  vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+end
+
+
 local nvim_tree = require("nvim-tree")
 nvim_tree.setup {
-  view = {
-    mappings = {
-      list = {
-        -- Remove / Replace default mappings
-        { key = "o", action = "" },
-        { key = "<C-t>", action = "vsplit" },
-        { key = "s", action = "vsplit" },
-        -- New mappings
-        { key = "l", action = "edit" },
-        { key = "h", action = "close_node" },
-      }
-    }
-  },
+  on_attach = my_on_attach,
   actions = {
     open_file = {
       window_picker = {
