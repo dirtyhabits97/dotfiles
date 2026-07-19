277e4a31c1a570df239d68a9f5375e94d27130f8
Gonzalo <gonzalorehu@gmail.com>
2026-07-19 16:33:37 +0800

    Silence a warning

--- config/nvim/lua/core/init.lua
+++ config/nvim/lua/core/init.lua
@@ -1,5 +1,9 @@
 -- vim.lsp.set_log_level("debug")
 
+-- shim deprecated vim.tbl_flatten (bufferline/lualine/lspconfig still call it)
+-- with a non-deprecated impl so no startup warning; drop when those plugins update.
+vim.tbl_flatten = function(t) return vim.iter(t):flatten(math.huge):totable() end
+
 require('core.plugins')
 require('core.set')
 require('core.mappings')
